f69b5d1c6edf393f425361d97daec0c186fd2b2b
Roman <nero26@bk.ru>
2022-12-20 19:09:12 +0300

    HomrWork_2 done

---
+++ Homework_2/Task_2.py
@@ -0,0 +1,12 @@
+# 2. Напишите программу, которая принимает на вход число N
+# и выдает набор произведений чисел от 1 до N в виде списка.
+# 1 - 1 * 1, 2 - 1 * 2, 3 - 1 * 2 * 3, 4 - 1 * 2 * 3 * 4 и т.д.
+# - 4 -> [1, 2, 6, 24]
+# - 6 -> [1, 2, 6, 24, 120, 720]
+
+n = int(input("Введите целое число => "))
+temporary_transferred = 1
+
+for i in range(n):
+    temporary_transferred = temporary_transferred * (i + 1)
+    print(temporary_transferred, end=" ")
